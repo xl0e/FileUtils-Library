71bc90be453f9d25c0a043923224e6dc1f87eb16
Mihail Gagauz <mihail.gagauz@zaelab.com>
2023-07-07 16:47:54 +0300

    Initial commit

---
+++ src/FileUtils.h
@@ -0,0 +1,85 @@
+#ifndef FileUtils_h
+#define FileUtils_h
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Logger.h>
+
+#ifndef SDCARD_SS
+#define SDCARD_SS 15
+#endif
+#define SDCARD_CLK 14
+#define SDCARD_MOSI 13
+#define SDCARD_MISO 12
+
+typedef void (*FileFunc)(File &file);
+
+class FileUtilsClass
+{
+private:
+  bool inited = false;
+  bool init()
+  {
+    if (!inited)
+    {
+      logger.println("Checking SD card... ");
+
+      if (!SD.begin(SDCARD_SS))
+      {
+        logger.println("Cannot read SD card");
+        return false;
+      }
+      logger.println("SD card is mounted");
+      inited = true;
+    }
+    return true;
+  }
+
+  void iterateRecursive(File dir, FileFunc fileFunc)
+  {
+    while (true)
+    {
+      File entry = dir.openNextFile();
+      if (!entry)
+      {
+        logger.println("no more files");
+        break;
+      }
+
+      if (entry.isDirectory())
+      {
+        iterateRecursive(entry, fileFunc);
+      }
+      else
+      {
+        fileFunc(entry);
+      }
+      entry.close();
+    }
+  }
+
+public:
+  File readFile(String fileName)
+  {
+    init();
+    return SD.open(fileName, FILE_READ);
+  }
+
+  void removeFile(String fileName)
+  {
+    init();
+    SD.remove(fileName);
+  }
+
+  void iterateFiles(String dirName, FileFunc fileFunc)
+  {
+    init();
+    File dir = SD.open(dirName);
+    iterateRecursive(dir, fileFunc);
+  }
+};
+
+extern FileUtilsClass FileUtils;
+
+#endif
